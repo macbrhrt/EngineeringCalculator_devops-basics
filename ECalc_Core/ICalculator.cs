324f6a477506b0bba079f385688740c121010fd8
f1acko <mikhov.2004@gmail.com>
2024-10-28 20:57:28 +0300

    Create Solution. Implement interfaces

---
+++ ECalc_Core/ICalculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECalc_Core
+{
+    public interface ICalculator
+    {
+        double Add(double a, double b);
+        double Subtract(double a, double b);
+        double Multiply(double a, double b);
+        double Divide(double a, double b);
+    }
+
+    public interface IScientificCalculator : ICalculator
+    {
+        double Power(double baseNum, double exponent);
+        double SquareRoot(double number);
+        double Logarithm(double number, double baseNum);
+        double NaturalLogarithm(double number);
+        double Sine(double angle);
+        double Cosine(double angle);
+        double Tangent(double angle);
+    }
+
+}
